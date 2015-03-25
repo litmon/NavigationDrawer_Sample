9b991a568ee3637f3a947b2a709abaa669b388ec
Fukuo Kadota <app.fk91@gmail.com>
2015-03-25 16:33:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fukuo/navigationdrawer_sample/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.fukuo.navigationdrawer_sample;
+
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+         * NavigationDrawer
+         */
+
+        DrawerLayout layout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, layout, R.string.title, R.string.title);
+        toggle.setDrawerIndicatorEnabled(true);
+        layout.setDrawerListener(toggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return toggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+}
