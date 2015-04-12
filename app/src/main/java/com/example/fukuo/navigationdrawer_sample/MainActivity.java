5289ef86c9b145af72210c1554b76155f7edc25d
Fukuo Kadota <app.fk91@gmail.com>
2015-04-12 23:45:42 +0900

    navigation drawer changed

--- app/src/main/java/com/example/fukuo/navigationdrawer_sample/MainActivity.java
+++ app/src/main/java/com/example/fukuo/navigationdrawer_sample/MainActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -40,6 +43,13 @@ public class MainActivity extends ActionBarActivity {
             adapter.add("a" + i);
 
         lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "listview clicked." + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
